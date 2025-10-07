e15e7006fbbf54b8eb10e7e33c744108bd1bcd3c
jambagigirish <jambagigirish@gmail.com>
2025-10-06 20:04:28 -0500

    Add files via upload

--- common/config.py
+++ common/config.py
@@ -1,1 +1,10 @@
 
+import os
+
+POSTGRES_DSN = os.getenv("POSTGRES_DSN", "postgresql+psycopg2://rfms:rfms@postgres:5432/rfms")
+S3_ENDPOINT   = os.getenv("S3_ENDPOINT", "http://minio:9000")
+S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY", "minioadmin")
+S3_SECRET_KEY = os.getenv("S3_SECRET_KEY", "minioadmin")
+S3_BUCKET     = os.getenv("S3_BUCKET", "rfms")
+REDIS_URL     = os.getenv("REDIS_URL", "redis://redis:6379/0")
+REGION        = os.getenv("REGION", "region-a")
